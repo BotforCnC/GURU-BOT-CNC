da4df8a8248b513323456fded0f6cbb64081fc70
BotforCnC <142037636+BotforCnC@users.noreply.github.com>
2023-08-20 20:01:10 +0530

    Update config.js

--- config.js
+++ config.js
@@ -7,8 +7,8 @@ import axios from 'axios'
 
 
 global.owner = [
-  ['94764497078', 'me', true],
-  ['6281255369012', 'Ash', true], 
+  ['94741351932', 'me', true],
+  ['94741351932', 'CN Harshana', true], 
   [''] 
 ] //Number of owners
 
